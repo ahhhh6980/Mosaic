136e3f29bf3bfb78e3725d1e1d585383eada0296
ahhhh6980 <ahhhh6980@gmail.com>
2022-02-19 07:36:25 -0500

    Can now pass in images of any shape, but palette must still be square

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use std::{path::Path, fs::read_dir, env};
+use std::{path::Path, fs::read_dir, env, cmp::max};
 use image::{ImageBuffer, Rgba, imageops::FilterType::Lanczos3};
 use glob::glob;
 use ndarray::{Array1};
@@ -35,25 +35,6 @@ fn compute_average_color(img: &ImageBuffer<Rgba<u8>, Vec<u8>>, w: usize, h: usiz
     Array1::from(avg.to_vec())
 }
 
-fn generate_palette(pname: &str, pw: u32, ph: u32) -> Vec<Label> {
-    let palette_path = format!("palettes/{}", pname);
-    let mut palette: Vec<Label> = vec![Label{color: Array1::<f32>::zeros(4), image: ImageBuffer::<Rgba<u8>, Vec<u8>>::new(1, 1)};1];
-    if Path::new(&palette_path).is_dir() {
-    
-        let set_count: usize = read_dir(&palette_path).unwrap().count() as usize;
-        palette = vec![Label{color: Array1::<f32>::zeros(4), image: ImageBuffer::<Rgba<u8>, Vec<u8>>::new(pw as u32, ph as u32)};set_count];
-
-        let images_paths = format!("{}/*", &palette_path);
-        for (i, item) in glob(&images_paths).expect("Error").enumerate() {
-            let item_name = item.unwrap();
-            let image = image::open(&item_name).unwrap().into_rgba8();
-            palette[i].color = compute_average_color(&image, pw as usize, ph as usize);
-            palette[i].image = image;
-        }
-    }
-    palette
-}
-
 fn bad_color_distance(pixel_a: &Array1<f32>, pixel_b: &Array1<f32>) -> u64 {
     let c1 = pixel_a / 256.0;
     let c2 = pixel_b / 256.0;
@@ -70,22 +51,64 @@ fn find_closest_image(pixel: &Array1<f32>, palette: &Vec<Label>) -> (ImageBuffer
     (label.image.clone(), label.color.clone())
 }
 
-fn generate_image(fname: &str, palette: &Vec<Label>, pw: u32, ph: u32, dw: u32, dh: u32) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+fn get_palette_dimensions(pname: &str) -> (usize, usize) {
+    let palette_path = format!("palettes/{}", pname);
+    let (mut w, mut h) = (0,0);
+    if Path::new(&palette_path).is_dir() {
+        let images_paths = format!("{}/*", &palette_path);
+        let image = image::open(glob(&images_paths).expect("Error").nth(0).unwrap().unwrap()).unwrap().into_rgba8();
+        w = image.width() as usize;
+        h = image.height() as usize;
+    }
+    (w, h)
+}
+
+fn resize_dims((mut w, mut h): (usize, usize), max_size: u32) -> (usize, usize) {
+    let max_dimension = max(w, h) as f32;
+    w = (((w as f32) / max_dimension) * (max_size as f32)) as usize;
+    h = (((h as f32) / max_dimension) * (max_size as f32)) as usize;
+    (w, h)
+}
+
+fn generate_palette(pname: &str, max_size: u32) -> Vec<Label> {
+    let (pw, ph) = resize_dims(get_palette_dimensions(&pname), max_size);
+
+    let palette_path = format!("palettes/{}", pname);
+    let mut palette: Vec<Label> = vec![Label{color: Array1::<f32>::zeros(4), image: ImageBuffer::<Rgba<u8>, Vec<u8>>::new(1, 1)};1];
+    if Path::new(&palette_path).is_dir() {
+    
+        let set_count: usize = read_dir(&palette_path).unwrap().count() as usize;
+        palette = vec![Label{color: Array1::<f32>::zeros(4), image: ImageBuffer::<Rgba<u8>, Vec<u8>>::new(pw as u32, ph as u32)};set_count];
+
+        let images_paths = format!("{}/*", &palette_path);
+        for (i, item) in glob(&images_paths).expect("Error").enumerate() {
+            let item_name = item.unwrap();
+            let image = image::open(&item_name).unwrap().resize(pw as u32, ph as u32, Lanczos3).into_rgba8();
+            palette[i].color = compute_average_color(&image, pw, ph);
+            palette[i].image = image;
+        }
+    }
+    palette
+}
+
+fn generate_image(fname: &str, pname: &str, palette: &Vec<Label>, pmax: u32, imax: u32) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
     let image_path = format!("input/{}", fname);
     let mut output = ImageBuffer::<Rgba<u8>, Vec<u8>>::new(1,1);
     if Path::new(&image_path).is_file() {
-        let image = image::open(&image_path).unwrap().resize(dw, dh, Lanczos3).into_rgba8();
-        let (w, h) = (image.width(), image.height());
-        output = ImageBuffer::<Rgba<u8>, Vec<u8>>::new(pw*w,ph*h);
+        let image = image::open(&image_path).unwrap();
+        let (w, h) = resize_dims((image.width() as usize, image.height() as usize), imax);
+        let (pw, ph) = resize_dims(get_palette_dimensions(&pname), pmax);
+        let image = image.resize(imax, imax, Lanczos3).into_rgba8();
+        output = ImageBuffer::<Rgba<u8>, Vec<u8>>::new((pw*w) as u32,(ph*h) as u32);
         for y in 0..h {
             for x in 0..w {
-                let temp = (image[(x,y)].0).to_vec();
+                let temp = (image[(x as u32,y as u32)].0).to_vec();
                 let pixel: Vec<f32> = temp.iter().map(|v| *v as f32).collect();
                 let pixel = Array1::<f32>::from(pixel);
                 let (pimage, pcolor) = find_closest_image(&pixel, &palette);
                 for oy in 0..ph {
                     for ox in 0..pw {
-                        let mut ppixel = pimage[(ox,oy)];
+                        let mut ppixel = pimage[(ox as u32,oy as u32)];
                         if ppixel[3] < 128 {
                             let value: u16 = pcolor.iter().map(|v| *v as u16).sum();
                             ppixel[0] = (value / 4) as u8;
@@ -93,7 +116,7 @@ fn generate_image(fname: &str, palette: &Vec<Label>, pw: u32, ph: u32, dw: u32, 
                             ppixel[2] = (value / 4) as u8;
                         }
                         ppixel[3] = pixel[3] as u8;
-                        output.put_pixel(x * pw + ox, y * ph + oy, ppixel);
+                        output.put_pixel((x * pw + ox) as u32, (y * ph + oy) as u32, ppixel);
                     }
                 }
             }
@@ -104,10 +127,10 @@ fn generate_image(fname: &str, palette: &Vec<Label>, pw: u32, ph: u32, dw: u32, 
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    let mut fname = "test.jpg";
+    let mut fname = "Landscape-Color.jpg";
     let mut pname = "emoji";
-    let mut fsize = 64;
-    let mut psize = 72;
+    let mut fsize = 256;
+    let mut psize = 16;
     for e in args.iter() {
         if e == "-p" {  pname = e;  }
         if e == "-f" {  fname = e;  }
@@ -116,11 +139,10 @@ fn main() -> std::io::Result<()> {
     }
     println!("Processing: {}, with palette: {}, at img size: {}, and palette size: {}", &fname, &pname, &fsize, &psize);
     let now = Instant::now();
-    let palette = generate_palette(pname, psize, psize);
-    let image = generate_image(fname, &palette, psize, psize, fsize, fsize);
-    //dbg!(image);
-    let save_name = format!("output/{}_p{}_f{}.{}", fname.split(".").collect::<Vec<&str>>()[0], psize, fsize, fname.split(".").collect::<Vec<&str>>()[1]);
-    image.save(save_name).unwrap();
+    let palette = generate_palette(pname, psize);
+    let image = generate_image(fname, pname, &palette, psize, fsize);
+    let save_name = format!("output/{}:{}_p{}_f{}.{}", fname.split(".").collect::<Vec<&str>>()[0], pname, psize, fsize, fname.split(".").collect::<Vec<&str>>()[1]);
+    image.save(save_name).expect("Error");
     println!("Finished in: {}ms", now.elapsed().as_millis());
     Ok(())
 }
